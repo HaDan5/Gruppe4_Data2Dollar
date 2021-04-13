55ca0b7460b55582843e25837c5a0c1bbfc2011a
Max <maximilian.thumm@googlemail.com>
2021-04-13 17:49:42 +0200

    Update
    Trying to select items and open; doesn't work; can't find error

--- immowelt/immowelt/spiders/getdata.py
+++ immowelt/immowelt/spiders/getdata.py
@@ -1,10 +1,39 @@
 import scrapy
+from scrapy.selector import Selector
+
+from selenium import webdriver
+from time import sleep
 
 
 class GetdataSpider(scrapy.Spider):
     name = 'getdata'
     allowed_domains = ['https://www.immowelt.de/']
-    start_urls = ['http://https://www.immowelt.de//']
+    start_urls = ['https://www.immowelt.de/']
 
     def parse(self, response):
-        pass
+        url = 'https://www.immowelt.de/liste/berlin/wohnungen/mieten?sort=price'
+        
+        #insert your driver path
+        self.driver = webdriver.Chrome('/Users/maxl/chromedriver/chromedriver')
+        #
+        #
+        #
+        self.driver.get(url)
+        #create Selector
+        sel = Selector(text=self.driver.page_source)
+        #liste aller Ergebnisse auf der Seite
+        items = sel.xpath('//*[@class="js-object   listitem_wrap "]')
+        for i in range(len(items)):
+        	self.driver.find_elements_by_xpath('//*[@class="listitem clear relative js-listitem "]/a')[i].click()
+        	sel = Selector(text=self.driver.page_source)
+        	title = sel.xpath('//*[@class="quickfacts iw_left"]/h1').extract()
+        	loc = sel.xpath('//*[@class="no_s"').extract()
+        	price = sel.xpath('//*[@class="hardfact"][1]/strong').extract()
+        	area = sel.xpath('//*[@class="hardfact"][2]').extract()
+        	rooms = sel.xpath('//*[@class="hardfact rooms"]').extract()
+        	sleep (2)
+        	self.driver.back()
+        	yield {'Name': title, 'Standort': loc, 'Kaltmiete': price, 'Wohnfläche': area, 'Zimmer': rooms}
+        	sleep (2)
+
+        self.driver.close()
