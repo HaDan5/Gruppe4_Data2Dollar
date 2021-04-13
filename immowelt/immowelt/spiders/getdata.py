498b33e7886dda8b39bc93c21bc0d1f71c950ee8
Max <maximilian.thumm@googlemail.com>
2021-04-13 16:50:37 +0200

    project updated

--- immowelt/immowelt/spiders/getdata.py
+++ immowelt/immowelt/spiders/getdata.py
@@ -1,23 +1,10 @@
 import scrapy
-from scrapy.selector import Selector
-
-from selenium import webdriver
-from time import sleep
 
 
 class GetdataSpider(scrapy.Spider):
     name = 'getdata'
     allowed_domains = ['https://www.immowelt.de/']
-    start_urls = ['https://www.immowelt.de/liste/berlin/wohnungen/mieten?sort=price']
+    start_urls = ['http://https://www.immowelt.de//']
 
     def parse(self, response):
-        url = 'https://www.immowelt.de/liste/berlin/wohnungen/mieten?sort=price'
-        
-        #insert your driver path
-        self.driver = webdriver.Chrome('/Users/maxl/chromedriver/chromedriver')
-        #
-        #
-        #
-        self.driver.get(url)
-        #create Selector
-        sel = Selector(text=self.driver.page_source)
+        pass
