450287e2bc958c7980e17ce56766e72420e7939c
Marc <marcoppenheim@Marcs-MacBook-Air-2.local>
2021-04-22 23:03:29 +0200

    Visualisierungen Matplotlib
    Code, dazugehörige Files & Visualisierungen

---
+++ Datenvisualisierung/Python_Code/Matplotlib_Visualisierung1.py
@@ -0,0 +1,39 @@
+import csv
+import numpy as np
+from collections import Counter
+from matplotlib import pyplot as plt
+
+
+
+plt.xkcd()
+
+with open("immowelt.csv") as csv_file:
+	csv_reader = csv.DictReader(csv_file)
+
+	Bezirke_Counter = Counter()
+
+	for row in csv_reader:
+		Bezirke_Counter.update(row["Bezirke"].split(";"))
+
+
+Bezirke = []
+Anzahl_Objekte = []
+
+for item in Bezirke_Counter.most_common(12):
+	Bezirke.append(item[0])
+	Anzahl_Objekte.append(item[1])
+
+
+Bezirke.reverse()
+Anzahl_Objekte.reverse()
+
+plt.barh(Bezirke, Anzahl_Objekte, color=["#FFC900", "#FFB500", "#FF9800", "#FF8000", "#FF7500", "#FF7100", "#FF6700", "#FF5800", "#FF5300", "#FF4500", "#FF2C00", "#FF0000"])
+
+plt.title("Verfügbare Objekte in Berlin")
+
+plt.xlabel("Anzahl Objekte")
+plt.tight_layout()
+
+
+plt.show()
+
---
+++ Datenvisualisierung/Python_Code/Matplotlib_Visualisierung2.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+plt.style.use("seaborn")
+
+data = pd.read_csv("Immowelt7.csv")
+Baujahr = data["Baujahr"]
+Mietpreise = data["Kaltmiete"]
+Wohnfläche = data["Wohnfläche"]
+
+plt.scatter(Mietpreise, Wohnfläche, c=Baujahr, cmap="afmhot", edgecolor="black", linewidth=1, alpha=0.75)
+
+cbar = plt.colorbar()
+cbar.set_label("Baujahr des Objektes")
+
+plt.xscale("log")
+plt.yscale("log")
+
+plt.title("Kaltmiete pro Wohnfläche & Baujahr")
+plt.xlabel("Kaltmiete")
+plt.ylabel("Wohnfläche")
+
+plt.tight_layout()
+
+plt.savefig("Visualisierung_2.png")
+plt.show()
